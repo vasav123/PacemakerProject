261987967fedae50ac477e86760d1a4e55d16782
Riaz Panjwani <Riaz@Riazs-MacBook-Pro.local>
2019-10-26 17:33:41 -0400

    Save user DCM settings

--- DCM/GUI.py
+++ DCM/GUI.py
@@ -3,6 +3,8 @@ import tkinter as tk
 import datetime
 import os
 from tkinter import messagebox
+from tkinter import Label
+import json
 
 class SampleApp(tk.Tk):
     def __init__(self):
@@ -43,6 +45,10 @@ class Welcome(tk.Frame):
         index = [x for x in range(0, len(content), 2) if inUsr.get() == content[x].strip('\n')]
         if index:
             if inPass.get() == content[index[0]+1].strip('\n'):
+                #store current user
+                open ("user.txt", "w").close
+                with open("user.txt", "a+") as file:
+                    file.write(inUsr.get())
                 tk.master.switch_frame(Main)
             else:
                 tk.Label(self, text="          Error: The password is incorrect.          ").grid(row=8, columnspan=2, pady=5)
@@ -87,42 +93,42 @@ class Main(tk.Frame):
         tk.Label(self, text="Atrial Pulse Width (ms)").grid(row=6)
         self.apw = tk.Entry(self)
         self.apw.grid(row=6, column=1)
-        self.apw_b = tk.Button(self, text="Set", command=lambda: self.set_avpw(self.apw, 3))
+        self.apw_b = tk.Button(self, text="Set", command=lambda: self.set_apw(self.apw, 3))
         #.grid(row=6, column=2)
 
         # Ventricular Pulse Width ======================================================================================
         tk.Label(self, text="Ventricular Pulse Width (ms)").grid(row=7)
         self.vpw = tk.Entry(self)
         self.vpw.grid(row=7, column=1)
-        self.vpw_b = tk.Button(self, text="Set", command=lambda: self.set_avpw(self.vpw, 4))
+        self.vpw_b = tk.Button(self, text="Set", command=lambda: self.set_vpw(self.vpw, 4))
         #.grid(row=7, column=2)
 
         # Atrial Pulse Amplitude Regulated =============================================================================
         tk.Label(self, text="Atrial Pulse Amplitude Regulated (V)").grid(row=8)
         self.apar = tk.Entry(self)
         self.apar.grid(row=8, column=1)
-        self.apar_b = tk.Button(self, text="Set", command=lambda: self.set_avpa(self.apar, 5))
+        self.apar_b = tk.Button(self, text="Set", command=lambda: self.set_apar(self.apar, 5))
         #.grid(row=8, column=2)
 
         # Ventricular Pulse Amplitude Regulated =======================================================================
         tk.Label(self, text="Ventricular Pulse Amplitude Regulated (V)").grid(row=9)
         self.vpar = tk.Entry(self)
         self.vpar.grid(row=9, column=1)
-        self.vpar_b = tk.Button(self, text="Set", command=lambda: self.set_avpa(self.vpar, 6))
+        self.vpar_b = tk.Button(self, text="Set", command=lambda: self.set_vpar(self.vpar, 6))
         #.grid(row=9, column=2)
 
         # Atrial Refractory Period ======================================================================================
         tk.Label(self, text="Atrial Refractory Period (ms)").grid(row=10)
         self.arp = tk.Entry(self)
         self.arp.grid(row=10, column=1)
-        self.arp_b = tk.Button(self, text="Set", command=lambda: self.set_avrp(self.arp, 7))
+        self.arp_b = tk.Button(self, text="Set", command=lambda: self.set_arp(self.arp, 7))
         #.grid(row=10, column=2)
 
         # Ventricular Refractory Period ============================================================================
         tk.Label(self, text="Ventricular Refractory Period (ms)").grid(row=11)
         self.vrp = tk.Entry(self)
         self.vrp.grid(row=11, column=1)
-        self.vrp_b = tk.Button(self, text="Set", command=lambda: self.set_avrp(self.vrp, 7))
+        self.vrp_b = tk.Button(self, text="Set", command=lambda: self.set_vrp(self.vrp, 7))
         #.grid(row=10, column=2)
 
         tk.Label(self, text="").grid(row=12)
@@ -198,6 +204,23 @@ class Main(tk.Frame):
         self.vpar.delete(0,'end')
         self.arp.delete(0,'end')
         self.vrp.delete(0,'end')
+        #Get User settings values
+        with open('user.txt', 'r') as file:
+            curruser = file.read()
+        filepath = curruser + '.json'
+        with open (filepath) as f:
+            usersettings = json.load(f)
+        LRLval = usersettings['LRL']
+        URLval = usersettings['URL']
+        APWval = usersettings['APW']
+        VPWval = usersettings['VPW']
+        APARval = usersettings['APAR']
+        VPARval = usersettings['VPAR']
+        ARPval = usersettings['ARP']
+        VRPval = usersettings['VRP']
+        
+        #Success message for mode selection
+        tk.Label(self, text= mode + " mode selected successfully").grid(row=6, column=4, rowspan=2,columnspan=4)
 
         if (mode == "AOO"):
             #Parameters that will show
@@ -215,10 +238,10 @@ class Main(tk.Frame):
             self.arp.delete(0,'end')
             self.vrp.delete(0,'end')
             #Default values to be inserted
-            self.lrl.insert(0, 'defaultvalue')
-            self.url.insert(0, 'defaultvalue')
-            self.apw.insert(0, 'defaultvalue')
-            self.apar.insert(0, 'defaultvalue')
+            self.lrl.insert(0, LRLval)
+            self.url.insert(0, URLval)
+            self.apw.insert(0, APWval)
+            self.apar.insert(0, APARval)
         if (mode == "VOO"):
             self.lrl_b.grid(row=4, column=2)
             self.url_b.grid(row=5, column=2)
@@ -234,10 +257,10 @@ class Main(tk.Frame):
             self.arp.delete(0,'end')
             self.vrp.delete(0,'end')
             #Default values to be inserted
-            self.lrl.insert(0, 'defaultvalue')
-            self.url.insert(0, 'defaultvalue')
-            self.vpw.insert(0, 'defaultvalue')
-            self.vpar.insert(0, 'defaultvalue')
+            self.lrl.insert(0, LRLval)
+            self.url.insert(0, URLval)
+            self.vpw.insert(0, VPWval)
+            self.vpar.insert(0, VPARval)
         if (mode == 'AAI'):
             self.lrl_b.grid(row=4, column=2)
             self.url_b.grid(row=5, column=2)
@@ -254,11 +277,11 @@ class Main(tk.Frame):
             self.arp.delete(0,'end')
             self.vrp.delete(0,'end')
             #Default values to be inserted
-            self.lrl.insert(0, 'defaultvalue')
-            self.url.insert(0, 'defaultvalue')
-            self.apw.insert(0, 'defaultvalue')
-            self.apar.insert(0, 'defaultvalue')
-            self.arp.insert(0, 'defaultvalue')
+            self.lrl.insert(0, LRLval)
+            self.url.insert(0, URLval)
+            self.apw.insert(0, APWval)
+            self.apar.insert(0, APARval)
+            self.arp.insert(0, ARPval)
         if (mode == "VVI"):
             self.lrl_b.grid(row=4, column=2)
             self.url_b.grid(row=5, column=2)
@@ -275,11 +298,11 @@ class Main(tk.Frame):
             self.arp.delete(0,'end')
             self.vrp.delete(0,'end')
             #Default values to be inserted
-            self.lrl.insert(0, 'defaultvalue')
-            self.url.insert(0, 'defaultvalue')
-            self.vpw.insert(0, 'defaultvalue')
-            self.vpar.insert(0, 'defaultvalue')
-            self.vrp.insert(0, 'defaultvalue')
+            self.lrl.insert(0, LRLval)
+            self.url.insert(0, URLval)
+            self.vpw.insert(0, VPWval)
+            self.vpar.insert(0, VPARval)
+            self.vrp.insert(0, VRPval)
         print (mode)
         return mode
     
@@ -290,41 +313,138 @@ class Main(tk.Frame):
     def set_lrl(self, value, port):
         if (value.get().isdigit()) and (int(value.get())>=30 and int(value.get())<=175):
             print("Sent " + value.get() + " to port " + str(port) + ".")
+            #Update json file value
+            with open('user.txt', 'r') as file:
+                curruser = file.read()
+            filepath = curruser + '.json'
+            with open (filepath) as f:
+                usersettings = json.load(f)
+            usersettings['LRL'] = value.get()
+            with open (filepath, "w") as file:
+                json.dump(usersettings, file)
         else:
             messagebox.showinfo("Error", "Please choose a value between 30ppm - 175ppm")
 
     def set_url(self, value, port):
         if (value.get().isdigit()) and (int(value.get())>=50 and int(value.get())<=175):
             print("Sent " + value.get() + " to port " + str(port) + ".")
+            #Update json file value
+            with open('user.txt', 'r') as file:
+                curruser = file.read()
+            filepath = curruser + '.json'
+            with open (filepath) as f:
+                usersettings = json.load(f)
+            usersettings['URL'] = value.get()
+            with open (filepath, "w") as file:
+                json.dump(usersettings, file)
         else:
             messagebox.showinfo("Error", "Please choose a value between 50ppm - 175ppm")
 
-    def set_avpw(self, value, port):
+    def set_apw(self, value, port):
         try:
             checknum = float(value.get())
             if (checknum>=0.1 and checknum<=1.9):
                 print("Sent " + value.get() + " to port " + str(port) + ".")
+                #Update json file value
+                with open('user.txt', 'r') as file:
+                    curruser = file.read()
+                filepath = curruser + '.json'
+                with open (filepath) as f:
+                    usersettings = json.load(f)
+                usersettings['APW'] = value.get()
+                with open (filepath, "w") as file:
+                    json.dump(usersettings, file)
+            else:
+                messagebox.showinfo("Error", "Please choose a value between 0.1ms - 1.9ms")
+        except ValueError:
+            messagebox.showinfo("Error", "Please choose a value between 0.1ms - 1.9ms")
+    
+    def set_vpw(self, value, port):
+        try:
+            checknum = float(value.get())
+            if (checknum>=0.1 and checknum<=1.9):
+                print("Sent " + value.get() + " to port " + str(port) + ".")
+                #Update json file value
+                with open('user.txt', 'r') as file:
+                    curruser = file.read()
+                filepath = curruser + '.json'
+                with open (filepath) as f:
+                    usersettings = json.load(f)
+                usersettings['VPW'] = value.get()
+                with open (filepath, "w") as file:
+                    json.dump(usersettings, file)
             else:
                 messagebox.showinfo("Error", "Please choose a value between 0.1ms - 1.9ms")
         except ValueError:
             messagebox.showinfo("Error", "Please choose a value between 0.1ms - 1.9ms")
 
-    def set_avpa(self, value, port):
+    def set_apar(self, value, port):
         try:
             checknum = float(value.get())
             if (checknum>=0.5 and checknum<=3.2) or (checknum>=3.5 and checknum<=7):
                 print("Sent " + value.get() + " to port " + str(port) + ".")
+                #Update json file value
+                with open('user.txt', 'r') as file:
+                    curruser = file.read()
+                filepath = curruser + '.json'
+                with open (filepath) as f:
+                    usersettings = json.load(f)
+                usersettings['APAR'] = value.get()
+                with open (filepath, "w") as file:
+                    json.dump(usersettings, file)
             else:
                 messagebox.showinfo("Error", "Please choose a value between 0.5V - 3.2V or 3.5V - 7.0V")
         except ValueError:
             messagebox.showinfo("Error", "Please choose a value between 0.5V - 3.2V or 3.5V - 7.0V")
 
-    def set_avrp(self, value, port):
+    def set_vpar(self, value, port):
+        try:
+            checknum = float(value.get())
+            if (checknum>=0.5 and checknum<=3.2) or (checknum>=3.5 and checknum<=7):
+                print("Sent " + value.get() + " to port " + str(port) + ".")
+                #Update json file value
+                with open('user.txt', 'r') as file:
+                    curruser = file.read()
+                filepath = curruser + '.json'
+                with open (filepath) as f:
+                    usersettings = json.load(f)
+                usersettings['VPAR'] = value.get()
+                with open (filepath, "w") as file:
+                    json.dump(usersettings, file)
+            else:
+                messagebox.showinfo("Error", "Please choose a value between 0.5V - 3.2V or 3.5V - 7.0V")
+        except ValueError:
+            messagebox.showinfo("Error", "Please choose a value between 0.5V - 3.2V or 3.5V - 7.0V")
+
+    def set_arp(self, value, port):
         if (value.get().isdigit()) and (int(value.get())>=150 and int(value.get())<=500):
             print("Sent " + value.get() + " to port " + str(port) + ".")
+            #Update json file value
+            with open('user.txt', 'r') as file:
+                curruser = file.read()
+            filepath = curruser + '.json'
+            with open (filepath) as f:
+                usersettings = json.load(f)
+            usersettings['ARP'] = value.get()
+            with open (filepath, "w") as file:
+                json.dump(usersettings, file)
         else:
             messagebox.showinfo("Error", "Please choose a value between 150ms - 500ms")
 
+    def set_vrp(self, value, port):
+        if (value.get().isdigit()) and (int(value.get())>=150 and int(value.get())<=500):
+            print("Sent " + value.get() + " to port " + str(port) + ".")
+            #Update json file value
+            with open('user.txt', 'r') as file:
+                curruser = file.read()
+            filepath = curruser + '.json'
+            with open (filepath) as f:
+                usersettings = json.load(f)
+            usersettings['VRP'] = value.get()
+            with open (filepath, "w") as file:
+                json.dump(usersettings, file)
+        else:
+            messagebox.showinfo("Error", "Please choose a value between 150ms - 500ms")
 
     def update_label(self):
         currentTime = datetime.datetime.now()
@@ -374,6 +494,12 @@ class Register(tk.Frame):
 
                 else:
                     tk.Label(self, text="          Account created succesfully.          ").grid(row=6, columnspan=2)
+                    #Create settings file for new user
+                    newUser = {'LRL': 30, 'URL': 50, 'APW': 0.1, 'VPW': 0.1, 'APAR':0.5, 'VPAR':0.5, 'ARP':150, 'VRP':150}
+                    filename = regUsr.get() + '.json'
+                    with open (filename, "w") as file:
+                        json.dump(newUser, file)
+                    #Add new user to accounts list
                     with open("accounts.txt", "a+") as file:
                         file.write(regUsr.get() + "\n")
                         file.write(regPass.get() + "\n")
